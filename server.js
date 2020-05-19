7a2e23157e6a758dccb17a82878004ed73ad0443
sidhik4742 <sidhik64742@gmail.com>
2020-05-19 12:11:26 +0530

    responsed link change

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.post('/index',function(req,res){
     message = req.body.comment;
     // console.log("FullName:"+fullName+"\nEmail:"+emailId+"\nMobileNo:"+mobileNo+"\nFeedback:"+message);
     // res.sendStatus(200).send(req.body);
-    res.send('<h5>Thanks for visiting my website and your valuable feedback! <a href=https://sidhik4742.github.io/MyPersonalWebsite/> You must click this link to back home page.</a></h5>');
+    res.send('<h5>Thanks for visiting my website and your valuable feedback! <a href=https://personal-websiteserver.herokuapp.com/> You must click this link to back home page.</a></h5>');
     res.end();
     var Data = {FullName:fullName,EmailId:emailId,MobileNumber:mobileNo,Message:message};
     console.log(Data);
@@ -65,7 +65,7 @@ app.post('/index',function(req,res){
         from: 'sidhh1994@gmail.com',
         to: emailId,
         subject: 'Thanks for your Feedback',
-        html:'<h2>S@R</h2><h5>Thanks for visiting my website and your valuable feedback! <a href=https://sidhik4742.github.io/MyPersonalWebsite/> You must click this link to back home page.</a></h5>'
+        html:'<h2>S@R</h2><h5>Thanks for visiting my website and your valuable feedback! <a href=https://personal-websiteserver.herokuapp.com/> You must click this link to back home page.</a></h5>'
       };
       
       transporter.sendMail(mailOptions, function(error, info){
